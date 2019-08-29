21238cf620561b750c3265b7d1f5c09a80f5e590
Johannes Grün <gruen.jojo.develop@gmail.com>
2019-08-29 15:58:18 +0200

    Bussim

--- src/main/java/edu/kit/ipd/sdq/modsim/descomp/services/Z3Test.java
+++ src/main/java/edu/kit/ipd/sdq/modsim/descomp/services/Z3Test.java
@@ -2,16 +2,11 @@ package edu.kit.ipd.sdq.modsim.descomp.services;
 
 import java.util.HashMap;
 
-import com.microsoft.z3.ArithExpr;
 import com.microsoft.z3.BoolExpr;
 import com.microsoft.z3.Context;
 import com.microsoft.z3.Expr;
 import com.microsoft.z3.IntExpr;
-import com.microsoft.z3.IntNum;
-import com.microsoft.z3.Model;
-import com.microsoft.z3.RealExpr;
 import com.microsoft.z3.Solver;
-import com.microsoft.z3.Status;
 
 public class Z3Test {
 
@@ -22,8 +17,33 @@ public class Z3Test {
 		Context ctx = new Context(cfg);
 		Solver s = ctx.mkSolver();
 
-		RealExpr delay = ctx.mkRealConst("delay");
-		RealExpr LOADING_TIME_PER_PASSENGER = ctx.mkRealConst("LOADING_TIME_PER_PASSENGER");
+		// BusStop position = bus.getPosition();
+		// int waitingPassengers = position.getWaitingPassengers();
+		//
+		// int servedPassengers = Math.min(waitingPassengers, bus.getTotalSeats());
+		//
+		// Utils.log(bus, "Loading " + servedPassengers + " passengers at bus stop " +
+		// position + "...");
+		// bus.load(servedPassengers);
+		//
+		// int remainingPassengers = waitingPassengers - servedPassengers;
+		// position.setWaitingPassengers(remainingPassengers);
+		//
+		// // wait until all passengers have entered the bus
+		// double loadingTime = servedPassengers *
+		// LOADING_TIME_PER_PASSENGER.toSeconds().value();
+		//
+		// // schedule load finished event
+		//// LoadFinishedEvent e = new LoadFinishedEvent(loadingTime,
+		// remainingPassengers, this.getModel(), "LoadFinished");
+		// e.schedule(bus, loadingTime);
+
+		// (declare-fun servedPassengers () Int) (declare-fun value () Int) (declare-fun
+		// totalSeats () Int) (declare-fun waitingPassengers () Int) (assert (=
+		// servedPassengers (ite (<= waitingPassengers totalSeats) waitingPassengers
+		// totalSeats)))
+
+		IntExpr value = ctx.mkIntConst("value");
 		IntExpr totalSeats = ctx.mkIntConst("totalSeats");
 		IntExpr waitingPassengers = ctx.mkIntConst("waitingPassengers");
 
@@ -33,23 +53,56 @@ public class Z3Test {
 
 		s.add(ctx.mkEq(servedPassengers, ite));
 
-		BoolExpr mkEq2 = ctx.mkEq(delay, ctx.mkMul(LOADING_TIME_PER_PASSENGER, servedPassengers));
+		BoolExpr mkEq2 = ctx.mkEq(value, ctx.mkSub(waitingPassengers, servedPassengers));
 
+		
 		s.add(mkEq2);
-
-		s.add(ctx.mkEq(LOADING_TIME_PER_PASSENGER, ctx.mkReal(3)));
+		System.out.println(s);
+		// s.add(ctx.mkEq(LOADING_TIME_PER_PASSENGER, ctx.mkReal(3)));
 		s.add(ctx.mkEq(totalSeats, ctx.mkInt(3)));
 		s.add(ctx.mkEq(waitingPassengers, ctx.mkInt(3)));
 		s.check();
 		s.getModel();
 		System.out.println(s);
-		
-		
+
+		// RealExpr delay = ctx.mkRealConst("delay");
+		// RealExpr LOADING_TIME_PER_PASSENGER =
+		// ctx.mkRealConst("LOADING_TIME_PER_PASSENGER");
+		// IntExpr totalSeats = ctx.mkIntConst("totalSeats");
+		// IntExpr waitingPassengers = ctx.mkIntConst("waitingPassengers");
+		//
+		// IntExpr servedPassengers = ctx.mkIntConst("servedPassengers");
+		//
+		// Expr ite = ctx.mkITE(ctx.mkLe(waitingPassengers, totalSeats),
+		// waitingPassengers, totalSeats);
+		//
+		// s.add(ctx.mkEq(servedPassengers, ite));
+		//
+		// BoolExpr mkEq2 = ctx.mkEq(delay, ctx.mkMul(LOADING_TIME_PER_PASSENGER,
+		// servedPassengers));
+		//
+		// s.add(mkEq2);
+		//
+		// s.add(ctx.mkEq(LOADING_TIME_PER_PASSENGER, ctx.mkReal(3)));
+		// s.add(ctx.mkEq(totalSeats, ctx.mkInt(3)));
+		// s.add(ctx.mkEq(waitingPassengers, ctx.mkInt(3)));
+		// s.check();
+		// s.getModel();
+		// System.out.println(s);
+
+		// RealExpr delay = ctx.mkRealConst("delay");
+		// IntExpr distance = ctx.mkIntConst("distance");
+		// IntExpr averageSpeed = ctx.mkIntConst("averageSpeed");
+		//
+		// s.add(ctx.mkEq(delay, ctx.mkDiv(distance, averageSpeed)));
+		// System.out.println(s);
 
 		// BoolExpr mkTrue = ctx.mkBool(true);
 		// BoolExpr mkFalse = ctx.mkBool(true);
 		// s.add(mkTrue);
-
+		// double drivingTime = Duration.hours(segment.getDistance() / (double)
+		// segment.getAverageSpeed()).toSeconds()
+		// .value();
 		// int waitingPassengers = position.getWaitingPassengers();
 		//
 		// int servedPassengers = Math.min(waitingPassengers, bus.getTotalSeats());
